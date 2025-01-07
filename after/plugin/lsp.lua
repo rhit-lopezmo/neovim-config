c45a2a83b87a26630ab2f187d3b2fd82ccdcd13f
rhit-lopezmo <lopezmo@rose-hulman.edu>
2025-01-07 01:20:51 -0500

    turned off c lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -75,7 +75,7 @@ require('lspconfig').gopls.setup({})
 
 require('lspconfig').clangd.setup({
     cmd = { "clangd" },
-    filetypes = { "c", "cpp", "objc", "objcpp" },
+    filetypes = { "cpp", "objc", "objcpp" },
     root_dir = require('lspconfig').util.root_pattern("compile_commands.json", "compile_flags.txt", ".git"),
 })
 
