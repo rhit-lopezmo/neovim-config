d6fab5786768c2d6f101e15c6b45b93552942894
rhit-lopezmo <lopezmo@rose-hulman.edu>
2025-01-09 02:57:30 -0500

    added color stuff for c

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -6,6 +6,7 @@ function colors_update(color)
 	vim.api.nvim_set_hl(0, "@variable", { fg = "#D0D0D0" })  -- Set the variable color to grey
 	vim.api.nvim_set_hl(0, "@parameter", { fg = "#D0D0D0" })  -- Set the parameter color to grey
 	vim.api.nvim_set_hl(0, "@variable.parameter", { fg = "#D0D0D0" })  -- Set the variable parameter color to grey
+	vim.api.nvim_set_hl(0, "@variable.parameter.c", { fg = "#D0D0D0" })  -- Set the variable parameter color to grey
 	vim.api.nvim_set_hl(0, "Comment", { fg = "#101010", italic = true })  -- Set the comment color to dark grey
 	vim.api.nvim_set_hl(0, "@punctuation.bracket", { fg = "#E5E500" })  
 	vim.api.nvim_set_hl(0, "@odp.punctuation.bracket.javascript", { fg = "#E5E500" }) 
